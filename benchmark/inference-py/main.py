5b21ea4612bb20b707cf8feb99025d2de96494c5
Robert Gibboni <robert@drivendata.org>
2020-08-12 13:55:17 -0700

    Simple test main.py that runs a few slides through alexnet

--- benchmark/inference-py/main.py
+++ benchmark/inference-py/main.py
@@ -1,98 +1,117 @@
 from datetime import datetime
 import logging
-import multiprocessing
 from pathlib import Path
+import random
 
-import cv2
-import numpy as np
-from PIL import ImageFile
 import pandas as pd
-from tensorflow.keras.applications import nasnet
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import pyvips
+import numpy as np
+import torch
+import torchvision
 
 # We get to see the log output for our execution, so log away!
 logging.basicConfig(level=logging.INFO)
 
-# This must be set to load some images using PIL, which Keras uses.
-ImageFile.LOAD_TRUNCATED_IMAGES = True
-
-ASSET_PATH = Path(__file__).parents[0] / "assets"
-MODEL_PATH = ASSET_PATH / "my_awesome_model.h5"
+ROOT_DIRECTORY = Path(__file__).parent.expanduser().resolve()
+# ROOT_DIRECTORY = Path(".").expanduser().resolve() / "benchmark" / "inference-py"
+ASSET_DIRECTORY = ROOT_DIRECTORY / "assets"
+MODEL_PATH = ASSET_DIRECTORY / "my-awesome-model.pt"
 
 # The images will live in a folder called 'data' in the container
-DATA_PATH = Path(__file__).parents[0] / "data"
+DATA_DIRECTORY = Path("/inference/data")
+# DATA_DIRECTORY = (
+#     Path("~/projects/sfp-cervical-biopsy-runtime/inference-data").expanduser().resolve()
+# )
+TILE_DIRECTORY = ROOT_DIRECTORY / "tiles"
+
+
+def generate_image_tiles(
+    input_directory: Path = DATA_DIRECTORY,
+    output_directory: Path = TILE_DIRECTORY,
+    tile_width: int = 512,
+    tile_height: int = 512,
+    tiles_per_level: int = 10,
+):
+    slide_indices = {}
+    for slide_index, path in enumerate(sorted(input_directory.glob("*.tif"))):
+        slide_indices[slide_index] = path.stem
+        (output_directory / path.stem).mkdir(exist_ok=True, parents=True)
+        for level in range(0, 3):
+            image = pyvips.Image.new_from_file(str(path), page=level)
+            for tile_index in range(tiles_per_level):
+                x = random.randrange(0, image.width - tile_width)
+                y = random.randrange(0, image.height - tile_height)
+                output_path = (
+                    output_directory / path.stem / f"level{level}_tile{tile_index}.tif"
+                )
+                image.crop(x, y, tile_width, tile_height).tiffsave(
+                    str(output_path), tile=False
+                )
+    return slide_indices
 
 
 def perform_inference():
-    """This is the main function executed at runtime in the cloud environment. """
+    """This is the main function executed at runtime in the cloud environment.
+    """
     logging.info("Loading model.")
-    model = load_model(MODEL_PATH)
+    model = torch.load(MODEL_PATH)
 
     logging.info("Loading and processing metadata.")
 
     # Our preprocessing selects the first image for each sequence
-    test_metadata = pd.read_csv(DATA_PATH / "test_metadata.csv", index_col="seq_id")
+    test_metadata = pd.read_csv(
+        DATA_DIRECTORY / "test_metadata.csv", index_col="seq_id"
+    )
     test_metadata = (
         test_metadata.sort_values("file_name").groupby("seq_id").first().reset_index()
     )
 
     # Prepend the path to our filename since our data lives in a separate folder
     test_metadata["full_path"] = test_metadata.file_name.map(
-        lambda x: str(DATA_PATH / x)
+        lambda x: str(DATA_DIRECTORY / x)
     )
 
     logging.info("Starting inference.")
 
     # Preallocate prediction output
-    submission_format = pd.read_csv(DATA_PATH / "submission_format.csv", index_col=0)
-    num_labels = submission_format.shape[1]
-    output = np.zeros((test_metadata.shape[0], num_labels))
+    submission_format = pd.read_csv(
+        DATA_DIRECTORY / "submission_format.csv", index_col="slide"
+    )
 
-    # Instantiate test data generator
-    datagen = ImageDataGenerator(preprocessing_function=nasnet.preprocess_input)
+    # Tile input images
+    slide_indices = generate_image_tiles()
 
-    batch_size = 256
-    test_datagen = datagen.flow_from_dataframe(
-        dataframe=test_metadata,
-        x_col="full_path",
-        y_col=None,
-        class_mode=None,
-        target_size=(224, 224),
-        batch_size=batch_size,
-        shuffle=False,
+    transform = torchvision.transforms.ToTensor()
+    image_folder = torchvision.datasets.ImageFolder(TILE_DIRECTORY, transform=transform)
+    data_generator = torch.utils.data.DataLoader(
+        image_folder, batch_size=4, shuffle=False
     )
 
     # Perform (and time) inference
-    steps = np.ceil(test_metadata.shape[0] / batch_size)
     inference_start = datetime.now()
-    preds = model.predict_generator(
-        test_datagen, steps=steps, verbose=1, workers=12, use_multiprocessing=False
-    )
-    inference_stop = datetime.now()
-    logging.info(f"Inference complete. Took {inference_stop - inference_start}.")
+    predictions = []
+    for batch, slides in data_generator:
+        preds = model.forward(batch)
+        for label, slide in zip(preds.argmax(1), slides):
+            predictions.append(
+                {"label": int(label), "slide": slide_indices[int(slide)]}
+            )
 
-    logging.info("Creating submission.")
+    logging.info(f"Inference complete. Took {datetime.now() - inference_start}.")
 
     # Check our predictions are in the same order as the submission format
-    assert np.all(
-        test_metadata.seq_id.unique().tolist() == submission_format.index.to_list()
-    )
-
-    output[: preds.shape[0], :] = preds[: output.shape[0], :]
-    my_submission = pd.DataFrame(
-        np.stack(output),
-        # Remember that we are predicting at the sequence, not image level
-        index=test_metadata.seq_id,
-        columns=submission_format.columns,
-    )
+    predictions = pd.DataFrame(predictions)
+    submission = predictions.groupby("slide").label.max()
+    logging.info("Creating submission.")
+    submission = submission.loc[submission_format.index]
+    assert (submission.index == submission_format.index).all()
 
     # We want to ensure all of our data are floats, not integers
-    my_submission = my_submission.astype(np.float)
+    submission = submission.astype(np.float)
 
     # Save out submission to root of directory
-    my_submission.to_csv("submission.csv", index=True)
-    logging.info(f"Submission saved.")
+    submission.to_csv("submission.csv", index=True)
+    logging.info("Submission saved.")
 
 
 if __name__ == "__main__":
