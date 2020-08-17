6fe75c103b3904a071f0430a6958337906d3b5ca
Robert Gibboni <robert@drivendata.org>
2020-08-17 10:18:53 -0700

    Test reading all image tiles

--- benchmark/inference-py/main.py
+++ benchmark/inference-py/main.py
@@ -1,11 +1,11 @@
 from datetime import datetime
+import itertools
 import logging
 from pathlib import Path
-import random
 
 import pandas as pd
+import PIL
 import pyvips
-import numpy as np
 import torch
 import torchvision
 
@@ -17,33 +17,65 @@ MODEL_PATH = ROOT_DIRECTORY / "assets" / "my-awesome-model.pt"
 
 # The images will live in a folder called '/inference/data/test_images' in the container
 DATA_DIRECTORY = Path("/inference/data")
-IMAGE_DIRECTORY = DATA_DIRECTORY / "test_images"
+IMAGE_DIRECTORY = DATA_DIRECTORY / "test_images" / "annotated_sample_tifs"
 TILE_DIRECTORY = ROOT_DIRECTORY / "tiles"
 
 
-def generate_image_tiles(
-    input_directory: Path = IMAGE_DIRECTORY,
-    output_directory: Path = TILE_DIRECTORY,
-    tile_width: int = 512,
-    tile_height: int = 512,
-    tiles_per_level: int = 10,
-):
-    slide_indices = {}
-    for slide_index, path in enumerate(sorted(input_directory.glob("*.tif"))):
-        slide_indices[slide_index] = path.stem
-        (output_directory / path.stem).mkdir(exist_ok=True, parents=True)
-        for level in range(0, 3):
-            image = pyvips.Image.new_from_file(str(path), page=level)
-            for tile_index in range(tiles_per_level):
-                x = random.randrange(0, image.width - tile_width)
-                y = random.randrange(0, image.height - tile_height)
-                output_path = (
-                    output_directory / path.stem / f"level{level}_tile{tile_index}.tif"
+class WholeSlideImageDataset(torch.utils.data.Dataset):
+    def __init__(
+        self,
+        metadata_path: Path,
+        image_directory: Path = IMAGE_DIRECTORY,
+        tile_width: int = 512,
+        tile_height: int = 512,
+        transform=None,
+    ):
+        metadata = pd.read_csv(metadata_path, index_col="filename")
+
+        indices = []
+        for entry in metadata.itertuples():
+
+            for row, column in itertools.product(
+                range(entry.width // tile_width), range(entry.height // tile_height)
+            ):
+                indices.append(
+                    {
+                        "filename": Path(entry.Index).with_suffix(".tif"),
+                        "row": row,
+                        "column": column,
+                    }
                 )
-                image.crop(x, y, tile_width, tile_height).tiffsave(
-                    str(output_path), tile=False
-                )
-    return slide_indices
+
+        logging.info(
+            "Dataset of %s images from %s", len(indices), image_directory,
+        )
+        self.indices = pd.DataFrame(indices)
+        self.image_directory = image_directory
+        self.tile_width = tile_width
+        self.tile_height = tile_height
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.indices)
+
+    def __getitem__(self, index):
+        index = self.indices.iloc[index]
+        logging.info("Reading %s %d %d", index.filename, index.column, index.row)
+        image = pyvips.Image.new_from_file(
+            str(self.image_directory / index.filename)
+        ).crop(
+            index.column * self.tile_width,
+            index.row * self.tile_height,
+            self.tile_width,
+            self.tile_height,
+        )
+        image = PIL.Image.frombuffer(
+            "RGB", (self.tile_width, self.tile_height), image.write_to_memory()
+        )
+        if self.transform is not None:
+            image = self.transform(image)
+
+        return image
 
 
 def perform_inference():
@@ -54,48 +86,48 @@ def perform_inference():
 
     logging.info("Loading and processing metadata.")
 
-    # Our preprocessing selects the first image for each sequence
-    test_metadata = pd.read_csv(DATA_DIRECTORY / "test_metadata.csv", index_col="slide")
+    transform = torchvision.transforms.ToTensor()
+    dataset = WholeSlideImageDataset(
+        DATA_DIRECTORY / "test_metadata.csv", transform=transform
+    )
 
     logging.info("Starting inference.")
     # Preallocate prediction output
     submission_format = pd.read_csv(
-        DATA_DIRECTORY / "submission_format.csv", index_col="slide"
+        DATA_DIRECTORY / "submission_format.csv", index_col="filename"
     )
 
-    # Tile input images
-    slide_indices = generate_image_tiles()
-
-    transform = torchvision.transforms.ToTensor()
-    image_folder = torchvision.datasets.ImageFolder(TILE_DIRECTORY, transform=transform)
-    data_generator = torch.utils.data.DataLoader(
-        image_folder, batch_size=4, shuffle=False
-    )
+    data_generator = torch.utils.data.DataLoader(dataset, batch_size=4, shuffle=False)
 
     # Perform (and time) inference
     inference_start = datetime.now()
+    logging.info("Starting inference %s", inference_start)
     predictions = []
-    for batch, slides in data_generator:
+    for batch in data_generator:
         preds = model.forward(batch)
-        for label, slide in zip(preds.argmax(1), slides):
-            predictions.append(
-                {"label": int(label), "slide": slide_indices[int(slide)]}
-            )
+        for label in preds.argmax(1):
+            predictions.append({"label": int(label)})
 
-    logging.info(f"Inference complete. Took {datetime.now() - inference_start}.")
+    inference_end = datetime.now()
+    logging.info(
+        "Inference complete at %s (duration %s)",
+        inference_end,
+        inference_end - inference_start,
+    )
 
-    # Check our predictions are in the same order as the submission format
-    predictions = pd.DataFrame(predictions)
-    submission = predictions.groupby("slide").label.max()
-    logging.info("Creating submission.")
-    submission = submission.loc[submission_format.index]
-    assert (submission.index == submission_format.index).all()
+    # # Check our predictions are in the same order as the submission format
+    # predictions = pd.DataFrame(predictions)
+    # submission = predictions.groupby("slide").label.max()
+    # logging.info("Creating submission.")
+    # submission = submission.loc[submission_format.index]
+    # assert (submission.index == submission_format.index).all()
 
-    # We want to ensure all of our data are floats, not integers
-    submission = submission.astype(np.float)
+    # # We want to ensure all of our data are floats, not integers
+    # submission = submission.astype(np.float)
 
     # Save out submission to root of directory
-    submission.to_csv("submission.csv", index=True)
+    # submission.to_csv("submission.csv", index=True)
+    submission_format.to_csv("submission.csv", index=True)
     logging.info("Submission saved.")
 
 
