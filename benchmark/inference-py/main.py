76b6b4d03e5eb2e22e57889a032c8b7f0b249f73
Robert Gibboni <robert@drivendata.org>
2020-08-24 17:12:58 -0700

    Only run tissue regions in benchmark

--- benchmark/inference-py/main.py
+++ benchmark/inference-py/main.py
@@ -1,11 +1,14 @@
 from datetime import datetime
-import itertools
 import logging
 from pathlib import Path
+from typing import Tuple
 
 import numpy as np
 import pandas as pd
 import pyvips
+from skimage.filters import threshold_otsu
+from skimage.color import rgb2hed
+from skimage.morphology import binary_dilation
 import torch
 import torchvision
 
@@ -20,7 +23,7 @@ DATA_DIRECTORY = Path("/inference/data")
 IMAGE_DIRECTORY = DATA_DIRECTORY / "test_images"
 
 
-def vips2numpy(fetch, image_format, width, height, bands):
+def vips2numpy(buffer, format: str, width: int, height: int, bands: int):
     format_to_dtype = {
         "uchar": np.uint8,
         "char": np.int8,
@@ -34,27 +37,98 @@ def vips2numpy(fetch, image_format, width, height, bands):
         "dpcomplex": np.complex128,
     }
     return np.ndarray(
-        buffer=fetch, dtype=format_to_dtype[image_format], shape=[height, width, bands],
+        buffer=buffer, dtype=format_to_dtype[format], shape=[height, width, bands],
     )
+
+
+def fetch_region_pyvips(
+    path: Path, location: Tuple[int, int], size: Tuple[int, int], level: int = 0
+):
+    """Loads image regions (faster than using pyvips.Image.crop)
+    """
+    image = pyvips.Image.new_from_file(str(path), page=level)
+    region = pyvips.Region.new(image)
+    return vips2numpy(
+        region.fetch(location[0], location[1], size[0], size[1]),
+        image.format,
+        size[0],
+        size[1],
+        image.bands,
+    )
+
+
+def get_tile_images(x, width, height):
+    n_rows, row_remainer = divmod(x.shape[0], height)
+    n_columns, column_remainder = divmod(x.shape[1], width)
+
+    assert (row_remainer == 0) and (
+        column_remainder == 0
+    ), "Array dimensions ({x.shape}) must be divisible by tile dimensions ({height}x{width})."
+
+    return np.lib.stride_tricks.as_strided(
+        np.ravel(x),
+        shape=(n_rows, n_columns, height, width),
+        strides=(height * x.strides[0], width * x.strides[1], *x.strides),
+        writeable=False,
+    )
+
+
+def get_tissue_mask(path: Path, level: int = 6):
+    """Creates a binary mask of tissue/non-tissue regions by using Otsu's method on the eosin
+    color channel"""
+    image = pyvips.Image.new_from_file(str(path), page=level)
+    array = vips2numpy(
+        image.write_to_memory(), image.format, image.width, image.height, image.bands
+    )
+    eosin = rgb2hed(array)[..., 1]
+    threshold = threshold_otsu(eosin)
+    return image, binary_dilation(eosin > threshold)
+
+
+def get_tissue_tile_indices(
+    path: Path,
+    level: int = 6,
+    tile_width: int = 512,
+    tile_height: int = 512,
+    threshold: float = 0.2,
+):
+    downsample_factor = 2 ** level
+    mask_tile_width, remainder = divmod(tile_width, downsample_factor)
+    assert (
+        remainder == 0
+    ), "Tile width {tile_width} not evenly divisible by downsample factor {downsample_factor}"
+    mask_tile_height, remainder = divmod(tile_height, downsample_factor)
+    assert (
+        remainder == 0
+    ), "Tile height {tile_height} not evenly divisible by downsample factor {downsample_factor}"
+
+    _, mask = get_tissue_mask(path, level)
+    n_columns = (mask.shape[1] - mask_tile_width) // mask_tile_width
+    n_rows = (mask.shape[0] - mask_tile_height) // mask_tile_height
+    tiles = get_tile_images(
+        mask[: n_rows * mask_tile_height, : n_columns * mask_tile_width].copy(),
+        mask_tile_width,
+        mask_tile_height,
+    )
+
+    return (tiles.mean((2, 3)) > threshold).nonzero()
 
 
 class WholeSlideImageDataset(torch.utils.data.Dataset):
     def __init__(
         self,
-        metadata_path: Path,
         image_directory: Path = IMAGE_DIRECTORY,
         tile_width: int = 512,
         tile_height: int = 512,
         transform=None,
     ):
-        metadata = pd.read_csv(metadata_path, index_col="filename")
-
         indices = []
         regions = []
-        for image_index, entry in enumerate(metadata.itertuples()):
-            image = pyvips.Image.new_from_file(
-                str(IMAGE_DIRECTORY / Path(entry.Index).with_suffix(".tif"))
-            )
+        assert (
+            image_directory.exists()
+        ), "Image directory {image_directory} does not exist"
+        for image_index, image_path in enumerate(image_directory.glob("*.tif")):
+            image = pyvips.Image.new_from_file(str(image_path))
             regions.append(
                 {
                     "region": pyvips.Region.new(image),
@@ -62,13 +136,10 @@ class WholeSlideImageDataset(torch.utils.data.Dataset):
                     "bands": image.bands,
                 }
             )
-            for row, column in itertools.product(
-                range(entry.width // (tile_width - 1)),
-                range(entry.height // (tile_height - 1)),
-            ):
+            for row, column in zip(*get_tissue_tile_indices(image_path)):
                 indices.append(
                     {
-                        "filename": Path(entry.Index).with_suffix(".tif"),
+                        "filename": image_path.name,
                         "image_index": image_index,
                         "row": row,
                         "column": column,
@@ -77,7 +148,7 @@ class WholeSlideImageDataset(torch.utils.data.Dataset):
 
         logging.info(
             "Dataset of %d images (%d tiles) images from %s",
-            len(metadata),
+            image_index + 1,
             len(indices),
             image_directory,
         )
@@ -96,17 +167,12 @@ class WholeSlideImageDataset(torch.utils.data.Dataset):
     def __getitem__(self, index):
         index = self.indices.iloc[index]
         region = self.regions[index.image_index]["region"]
-        try:
-            tile = region.fetch(
-                index.column * self.tile_width,
-                index.row * self.tile_height,
-                self.tile_width,
-                self.tile_height,
-            )
-        # until we fix the width and height in the metadata
-        except pyvips.error.Error:
-            tile = region.fetch(0, 0, self.tile_width, self.tile_height)
-
+        tile = region.fetch(
+            index.column * self.tile_width,
+            index.row * self.tile_height,
+            self.tile_width,
+            self.tile_height,
+        )
         tile = vips2numpy(
             tile,
             self.regions[index.image_index]["format"],
@@ -118,7 +184,7 @@ class WholeSlideImageDataset(torch.utils.data.Dataset):
         if self.transform is not None:
             tile = self.transform(tile)
 
-        return tile, index.filename.name
+        return tile, index.filename
 
 
 def perform_inference(batch_size: int = 16):
@@ -129,12 +195,10 @@ def perform_inference(batch_size: int = 16):
     if torch.cuda.is_available():
         model = model.to("cuda")
 
-    logging.info("Loading and processing metadata.")
+    logging.info("Loading and processing dataset.")
 
     transform = torchvision.transforms.ToTensor()
-    dataset = WholeSlideImageDataset(
-        DATA_DIRECTORY / "test_metadata.csv", transform=transform
-    )
+    dataset = WholeSlideImageDataset(transform=transform)
 
     logging.info("Starting inference.")
     # Preallocate prediction output
