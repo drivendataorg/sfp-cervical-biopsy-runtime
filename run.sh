ba5c4adafb461cdb4706f4b06218923194657205
Robert Gibboni <robert@drivendata.org>
2020-08-25 16:24:50 -0700

    Tag docker containers run during tests

--- run.sh
+++ run.sh
@@ -9,16 +9,20 @@ fi
 # test configuration
 if [ $(which nvidia-smi) ]
 then
+    docker rm -f sfp-submission-gpu
     docker build --build-arg CPU_GPU=gpu -t sfp-cervical-biopsy/inference runtime
     docker run \
+	   --name sfp-submission-gpu \
 	   --gpus all \
            --network none \
            --mount type=bind,source=$(pwd)/inference-data,target=/inference/data,readonly \
            --mount type=bind,source=$(pwd)/submission,target=/inference/submission \
            sfp-cervical-biopsy/inference
 else
+    docker rm -f sfp-submission-cpu
     docker build --build-arg CPU_GPU=cpu -t sfp-cervical-biopsy/inference runtime
     docker run \
+	   --name sfp-submission-cpu \
 	   --network none \
            --mount type=bind,source=$(pwd)/inference-data,target=/inference/data,readonly \
            --mount type=bind,source=$(pwd)/submission,target=/inference/submission \
